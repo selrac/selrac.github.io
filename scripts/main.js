0a81ee1038d9a6eb6ff6e71f9a5b5f25b21b7189
selrac <carlessg11@gmail.com>
2019-05-02 00:13:36 +0200

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/geometry.png') {
+      myImage.setAttribute ('src','images/cool_cat.jpg');
+    } else {
+      myImage.setAttribute ('src','images/geometry.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
+
+if(!localStorage.getItem('name')) 
+{
+    setUserName();
+}
+else
+{
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
